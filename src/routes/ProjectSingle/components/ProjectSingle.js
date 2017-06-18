fbd4b25b3fb673ca22c32d7376f0ebac098ae3f4
Zach Shaw <zachshaw1@gmail.com>
2017-06-18 13:57:35 +0100

    adding new projectsingle componetn

---
+++ src/routes/ProjectSingle/components/ProjectSingle.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class ProjectSingle extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount() {
+  }
+    
+  render() {
+    
+    return (
+      <div>
+        <h1>Project Single!!</h1>
+      </div>
+    )
+  }
+}
+
+export default ProjectSingle
