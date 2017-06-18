1561a2723bb177ff8fae55d9a63d8c12a03d139f
Zach Shaw <zachshaw1@gmail.com>
2017-06-18 19:28:42 +0100

    more information added to pages, mostly around cv and stack

--- src/routes/About/components/About.js
+++ src/routes/About/components/About.js
@@ -1,9 +1,34 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './About.scss'
 
 export const About = ({}) => (
   <div style={{ margin: '0 auto' }} >
-    <h2>About</h2>
+    <h3>Experience</h3>
+    <div>
+      <h4>Front End Engineer</h4>
+      <p>Where: Play, August 2016 - Present</p>
+      <p>Role: Building the next generation of scalable Health and Beauty apps for Boots as part of a small team collectively creating 5 different AngularJS 1.5 applications using component based architecture and ES6.
+      </p>
+    </div>
+    <div>
+      <h4>Front End Engineer</h4>
+      <p>Where: Betalab (Boots UK), November 2015 - August 2016</p>
+      <p>Role: Building the next generation of scalable Health and Beauty apps for Boots as part of a small team collectively creating 5 different AngularJS 1.5 applications using component based architecture and ES6.
+      </p>
+    </div>
+    <div>
+      <h4>Digital Engineer</h4>
+      <p>Where: Betalab (Boots UK), April 2015 - October 2015</p>
+      <p>Role: Re-engineering order and collect from conception to production, scoping digital solutions that were fit for purpose with a focus on improving CX. Research on the latest technologies, integration between enterprise systems and 3rd party API's,workflows, option analysis, UX and architecture.
+      </p>
+    </div>
+    <div>
+      <h4>SAP Developer</h4>
+      <p>Where: Boots UK, August 2013 - April 2015</p>
+      <p>Role: ABAP Developer working on custom interfaces, reports and extensions for different end users & teams. This includes Finance, HR, Supply Chain, Pharmacy and Real Estate as well as gaining extra skills in automation tool script writing for mass data migration and maintenance.</p>
+    </div>
+    <h2>Away from my laptop</h2>
   </div>
 )
 
--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -5,7 +5,9 @@ import './HomeView.scss'
 export const HomeView = () => (
   <div className="home-container">
     <h1>Zach Shaw</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <h2>Web Developer</h2>
+    <h3>Stack</h3>
+    <p> React, AngularJS, Redux, Javascript (ES6), jQuery, webpack, gulp, grunt, Sass, Less, Stylus, yarn, bower, npm, webpack, Chai, AWS, Semaphore CI, Google Analytics, JIRA, Confluence, Material UI, Bootstrap</p>
     <div className="btn-container">
       <button>Projects</button>
       <button>About</button>
