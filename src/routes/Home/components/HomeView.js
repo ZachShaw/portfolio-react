69096de5640545031c23d4ffc39ff86212df8f2a
Zach <zachshaw1@gmail.com>
2017-06-21 10:21:03 +0100

    sped up animation time for experience page

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -8,13 +8,13 @@ export const HomeView = () => (
     <div style={{ width: '100%' }}>
       <h1>Zach Shaw</h1>
       <h4>Front End Engineer</h4>
-      <p>Stack: React, AngularJS, Redux, Javascript (ES6), jQuery, webpack, gulp, grunt, Sass, Less, Stylus, yarn, bower, npm, webpack, Chai, AWS, Semaphore CI, Google Analytics, JIRA, Confluence, Material UI, Bootstrap</p>
+      <p>Stack: React, AngularJS, Redux, Javascript (ES6), jQuery, REST, webpack, gulp, grunt, Sass, Less, Stylus, yarn, bower, npm, webpack, pug, Chai, AWS, Semaphore CI, Google Analytics, JIRA, Confluence, Material UI, Bootstrap</p>
       <div className="btn-container">
         <Link to="/projects"><button>Projects</button></Link>
         <Link to="/about"><button>Experience</button></Link>
         <button>Contact</button>
       </div>
-    </div>  
+    </div>
   </div>
 )
 
