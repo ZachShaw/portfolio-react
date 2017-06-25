e476adcfbaf746c87cc8bd36fd0bf7f067156ed6
Zach Shaw <zachshaw1@gmail.com>
2017-06-25 18:10:59 +0100

    styled project single page and contact page complete

--- src/routes/Contact/components/Contact.js
+++ src/routes/Contact/components/Contact.js
@@ -6,14 +6,10 @@ export const Contact = ({}) => (
   <div className="contact-container" >
     <h2>Contact</h2>
     <div className="contact-wrapper">
-      <img className="img-email" src={require(`../../../assets/images/email.svg`)} />
-      <p>Feel free to email me at zachshaw1@gmail.com</p>
+      <p>Feel free to email me at <a href="mailto:zachshaw1@gmail.com">zachshaw1@gmail.com</a></p>
     </div>
     <div className="contact-wrapper">
-      <div className="coffee-wrapper">
-        <img className="img-coffee" src={require(`../../../assets/images/coffee.svg`)} />
-      </div>
-      <p>If you love coffee as much as me then let's definitely go for one :)</p>
+      <p>If you love coffee as much as me then let's definitely go for one <span><img className="img-coffee" src={require(`../../../assets/images/coffee-mug.svg`)} /></span></p>
     </div>
   </div>
 )
