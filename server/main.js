9f4e47e4450aa78cb9614b71f06a9f5ffa8ecc19
Zach Shaw <zachshaw1@gmail.com>
2017-07-01 14:11:27 +0100

    updating express to build to dist folder again

--- server/main.js
+++ server/main.js
@@ -66,7 +66,7 @@ if (project.env === 'development') {
 
 // Always return the main index.html
 app.get('*', function (request, response){
-    response.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+    response.sendFile(path.resolve(__dirname, './dist/index.html'))
 }) 
 
 module.exports = app
