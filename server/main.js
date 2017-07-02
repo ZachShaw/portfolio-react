bf6c55520008f01e9fbbf3fc206d37cfdd396442
Zach Shaw <zachshaw1@gmail.com>
2017-07-02 19:44:23 +0100

    testing server config

--- server/main.js
+++ server/main.js
@@ -65,7 +65,7 @@ if (project.env === 'development') {
 
   // Always return the main index.html
   app.get('*', function (request, response){
-      response.sendFile(path.resolve(__dirname + '../dist/index.html'));
+      response.sendFile(path.resolve(__dirname + '/../dist/index.html'));
   }) 
 }
 
