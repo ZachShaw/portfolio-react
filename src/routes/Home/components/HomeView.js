4669687bf82585d3db2f408addb800f3d78796f2
Zach <zachshaw1@gmail.com>
2017-07-04 10:30:22 +0100

    adding max widths to elements on landing

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -8,7 +8,7 @@ export const HomeView = () => (
     <div style={{ width: '100%' }}>
       <h1>Zach Shaw</h1>
       <h4>Front End Engineer</h4>
-      <p>Stack: React, AngularJS, Redux, Javascript (ES6), REST, webpack2, Sass, Less, Stylus, yarn, npm, webpack, pug, Chai, jQuery, AWS, Heroku, Semaphore CI, Google Analytics, JIRA, Confluence, Material</p>
+      <p>React, AngularJS, Redux, JavaScript (ES6), REST, webpack2, Sass, Less, Stylus, yarn, npm, webpack, pug, Chai, jQuery, AWS, Heroku, Semaphore CI, Google Analytics, JIRA, Confluence, Material</p>
       <div className="btn-container">
         <Link to="/projects"><div className="btn">Projects</div></Link>
         <Link to="/experience"><div className="btn">Experience</div></Link>
