4dc814e596624592a9a52d0b0def167098391adb
Zach <zachshaw1@gmail.com>
2017-07-07 16:50:21 +0100

    added london location and icon

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -8,6 +8,9 @@ export const HomeView = () => (
     <div style={{ width: '100%' }}>
       <h1>Zach Shaw</h1>
       <h4>Front End Engineer</h4>
+      <div className="location">
+        <img className="back" src={require(`../../../assets/images/location.svg`)} /><h5>London</h5>
+      </div>
       <p>React, AngularJS, Redux, JavaScript (ES6), REST, webpack2, Sass, Less, Stylus, yarn, npm, webpack, pug, Chai, jQuery, AWS, Heroku, Semaphore CI, Google Analytics, JIRA, Confluence, Material</p>
       <div className="btn-container">
         <Link to="/projects"><div className="btn">Projects</div></Link>
