f70f88dd727aed009a6c47e2cb3da8c0e976ddc9
Zach <zachshaw1@gmail.com>
2017-07-28 09:25:36 +0100

    updating contact

--- src/routes/Contact/components/Contact.js
+++ src/routes/Contact/components/Contact.js
@@ -6,7 +6,7 @@ export const Contact = ({}) => (
   <div className="contact-container" >
     <h2>Contact</h2>
     <div className="contact-wrapper">
-      <p>Feel free to email me at <a href="mailto:zachshaw1@gmail.com">zachshaw1@gmail.com</a></p>
+      <p>For contract or freelance work please contact me at <a href="mailto:zachshaw1@gmail.com">zachshaw1@gmail.com</a></p>
     </div>
     <div className="contact-wrapper">
       <p>If you love coffee as much as me then let's definitely go for one <span><img className="img-coffee" src={require(`../../../assets/images/coffee-mug.svg`)} /></span></p>
