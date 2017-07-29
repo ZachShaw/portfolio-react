e9eeaa604b0092294f91de96f8a4a92bb82b808c
Zach Shaw <zachshaw1@gmail.com>
2017-07-29 12:05:43 +0100

    initial test checking enzyme is working correctly

--- test/Contact/Contact-test.js
+++ test/Contact/Contact-test.js
@@ -9,6 +9,16 @@ describe("A suite", () => {
     expect(wrapper.containsMatchingElement(<p>hello</p>)).to.eq(true);
   });
 
+  it("should have an email anchor tag linking to zachshaw1@gmail.com", () => {
+    const wrapper = shallow(<Contact />);
+
+    expect(
+      wrapper
+        .find('a')
+        .prop('href')
+    ).to.eq('mailto:zachshaw1@gmail.com')
+  });
+
   it("should work  with a component", () => {
     const wrapper = shallow(<Contact />);
     const expected = wrapper.containsMatchingElement(<h2>Contact</h2>);
